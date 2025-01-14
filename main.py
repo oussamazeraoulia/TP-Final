d714e1e8d367c9a35bf1426c43f555e70c797c52
oussama zeraoulia <zeraouliaoussama40@gmail.com>
2025-01-14 18:01:36 +0100

    change 13

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-from fastapi import FastAPI, Request
+from fastapi import FastAPI, Request, File, UploadFile
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
 from fastapi.staticfiles import StaticFiles
-
+import os
 
 app = FastAPI()
 
@@ -14,13 +14,24 @@ async def index(request: Request):
     context = {"request": request, "title": "Page pricipale"}
     return templates.TemplateResponse("index.html", context)
 
-@app.get("/chatbot/", response_class=HTMLResponse)
+@app.get("/chatbot/")
 async def chatbot(request: Request):
     context = {"request": request, "title": "Chatbot"}
-    return templates.TemplateResponse("chatbot.html", context)
+    file_path = os.path.join("templates", "chatbot.html")
+    # return templates.TemplateResponse("chatbot.html", context)
+    return HTMLResponse(open(file_path).read())
 
 
 @app.get("/recherche/", response_class=HTMLResponse)
 async def recherche(request: Request):
     context = {"request": request, "title": "Recherche dans un document"}
     return templates.TemplateResponse("recherche.html", context)
+
+
+uploaded_content = ""
+
+@app.post("/upload/")
+async def upload_file(file: UploadFile = File(...)):
+    global uploaded_content
+    uploaded_content = (await file.read()).decode("utf-8")
+    return {"filename": file.filename, "content": uploaded_content}
