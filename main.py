45c7ea360153af500b4289bb777dcc702c2970c0
oussama zeraoulia <zeraouliaoussama40@gmail.com>
2025-01-14 15:15:31 +0100

    Describe your changes

--- main.py
+++ main.py
@@ -1,15 +1,12 @@
 from fastapi import FastAPI, Request
-from fastapi.responses import HTMLResponse
-from fastapi.templating import Jinja2Templates
-from fastapi.staticfiles import StaticFiles
+
 
 
 app = FastAPI()
 
-app.mount("/static", StaticFiles(directory="static"), name="static")
-templates = Jinja2Templates(directory="templates")
 
-@app.get("/", response_class=HTMLResponse)
-async def read_root(request: Request):
-    context = {"request": request, "title": "Welcome to FastAPI"}
-    return templates.TemplateResponse("index.html", context)
+
+@app.get("/")
+async def read_root():
+    context = {"title": "Welcome to FastAPI"}
+    return 'oussama 50'
