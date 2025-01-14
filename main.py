c91d63339391dfdb5ba0dfa3edefaf1b31a0b04b
oussama zeraoulia <zeraouliaoussama40@gmail.com>
2025-01-14 19:05:05 +0100

    change 20

--- main.py
+++ main.py
@@ -26,14 +26,14 @@ async def recherche(request: Request):
     return templates.TemplateResponse("recherche.html", context)
 
 
-uploaded_content = ""
+# uploaded_content = ""
 
-@app.post("/upload/")
-async def upload_file(file: UploadFile = File(...)):
-    uploaded_content = ""
-    # global uploaded_content
-    uploaded_content = (await file.read()).decode("utf-8")
-    return {"filename": file.filename, "content": uploaded_content}
+# @app.post("/upload/")
+# async def upload_file(file: UploadFile = File(...)):
+#     uploaded_content = ""
+#     # global uploaded_content
+#     uploaded_content = (await file.read()).decode("utf-8")
+#     return {"filename": file.filename, "content": uploaded_content}
 
 
 # @app.post("/upload/")
